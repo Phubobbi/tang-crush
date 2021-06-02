f67afd2ff5811016f7057385952e8200a7e68b4c
Phú Nhí <85255872+Phubobbi@users.noreply.github.com>
2021-06-03 03:23:41 +0700

    Rename config.js to bobbi.js

---
+++ bobbi.js
@@ -0,0 +1,22 @@
+
+const CONFIG = {
+    introTitle: 'Nhùn à!',
+    introDesc: `Trái đất này vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+   Để tim anh lẻ loi
+    Đằng sau chữ bạn không thể đến được chữ thương,
+    btnIntro: 'hhuhu',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Bobbi à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em có thể yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Bobbi biết mà 🥰. Yêu nhùn nhiều nhiều 😘😘',
+    messDesc: 'Hết dịch đi xem phim nhá bae <3<3.',
+    btnAccept: 'Gật gật <3',
+    messLink: 'http://fb.com' //https://m.me/queanhchi90.
+}
+```
+
